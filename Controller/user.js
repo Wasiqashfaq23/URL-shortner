77f9e3dc00bb795f2067e157c7ce28d6269a7692
Wasiq Ashfaq <wasiqashfaq17@gmail.com>
2026-05-01 21:09:58 +0500

    addded error for existing user

--- Controller/user.js
+++ Controller/user.js
@@ -5,6 +5,10 @@ const { setUser } = require("../Service/Auth")
 
 async function handleSignup(req, res) {
     const { userName, email, password } = req.body;
+    const user=await User.findOne({email})
+    if(user){
+        return res.render("signup",{error:"User with this email already exists"})
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     await User.create({
         userName,
